4a33ec40e7616c9d84a8f68936d01fd6d5aa00ae
jose <j.campos@estudiantec.cr>
2025-05-17 18:21:42 -0600

    final

--- Problem_1/AsamProb1.s
+++ Problem_1/AsamProb1.s
@@ -1,32 +1,33 @@
 .global _start
 
 .section .data
-array:    .word  1, 2, 3, 4, 5, 6, 7, 8, 9, 10  
+array:    .word  1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 
 .section .text
 _start:
-    mov r0, #0              @ índice
-    ldr r1, =array          @ dirección base
-    mov r4, #5              @ constante
+    mov r0, #0              
+    ldr r1, =array          
+    mov r4, #5              
 
 LOOP:
-    cmp r0, #10
-    bge END
+    cmp r0, #10             
+    bge END                 
 
-    ldr r2, [r1, r0, lsl #2]
-    cmp r2, r4
-    bge MULTIPLY
+    ldr r2, [r1, r0, lsl #2]  
+    cmp r2, r4              
+    bge MULTIPLY            
 
-    add r2, r2, r4
-    str r2, [r1, r0, lsl #2]
-    add r0, r0, #1
-    b LOOP
+    add r2, r2, r4          
+    str r2, [r1, r0, lsl #2] 
+    add r0, r0, #1          
+    b LOOP                  
 
 MULTIPLY:
-    mul r2, r2, r4
-    str r2, [r1, r0, lsl #2]
-    add r0, r0, #1
-    b LOOP
+    mul r2, r2, r4           
+    str r2, [r1, r0, lsl #2] 
+    add r0, r0, #1          
+    b LOOP                  
 
 END:
-    b END
+    b END                   
+
--- Problem_2/Factorial.s
+++ Problem_2/Factorial.s
@@ -2,14 +2,14 @@
 
 
 _start:
-	mov r0, #5 
-	mov r2, r0
+	mov r0, #5 @valor x aqui se mueve el que se quiere
+	mov r2, r0 @valor inicial
 
 LOOP:
-cmp r0, #1
-ble END
-sub r0, r0, #1
-mul r2, r2, r0
+cmp r0, #1 @compara si ya estamos en 1
+ble END @se va a end
+sub r0, r0, #1 @le resta 1 osea x-1
+mul r2, r2, r0 @lo multiplica 
 
 b LOOP
 
